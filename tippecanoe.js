2238e691a411dd2feec8c2870b2c45eb72b640b4
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-04-05 16:59:23 -0400

    update

--- tippecanoe.js
+++ tippecanoe.js
@@ -5,15 +5,17 @@ const keys = Object.keys(groups)
 const dstDir = config.get('dstDir')
 
 for (let key of keys) {
+  const maxzoom = key === 'ocean' ? 13 : 15
   const tippecanoe = spawnSync('tippecanoe', [
     '--force',
     '--simplification=2',
     `--output=${dstDir}/${key}.mbtiles`,
     '--minimum-zoom=2',
-    '--maximum-zoom=15',
+    `--maximum-zoom=${maxzoom}`,
     '--hilbert',
     '--detect-shared-borders',
     '--detect-longitude-wraparound',
+    '--no-tile-size-limit',
     `${dstDir}/${key}.rfc8142`
   ], { stdio: 'inherit' })
 }
